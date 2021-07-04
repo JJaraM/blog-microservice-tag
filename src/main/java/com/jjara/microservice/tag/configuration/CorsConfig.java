770498f0bc5e10dd400590e79c3750e2f205a0de
Jonathan Jara Morales <jonathanjaramorales@Jonathans-MacBook-Pro.local>
2021-07-04 19:39:21 +0100

    remove cors

--- src/main/java/com/jjara/microservice/tag/configuration/CorsConfig.java
+++ src/main/java/com/jjara/microservice/tag/configuration/CorsConfig.java
@@ -1,31 +1,31 @@
-package com.jjara.microservice.tag.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class CorsConfig {
-
-    /**
-     * CORS configuration that we need to access this microservice operations for
-     * external applications.
-     *
-     * @return an instance of cross configuration filter
-     */
-    @Bean
-    public CorsWebFilter corsFilter() {
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addAllowedOrigin("*");
-
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
-    }
-}
+//package com.jjara.microservice.tag.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    /**
+//     * CORS configuration that we need to access this microservice operations for
+//     * external applications.
+//     *
+//     * @return an instance of cross configuration filter
+//     */
+//    @Bean
+//    public CorsWebFilter corsFilter() {
+//        final CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.addAllowedOrigin("*");
+//
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
