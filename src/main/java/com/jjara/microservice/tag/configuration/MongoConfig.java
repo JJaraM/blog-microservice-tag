8e19a71586a07116d925e95fa03fed9a2eeba8d9
Jonathan <jonathanjm100@gmail.com>
2022-07-02 08:40:16 +0100

    Change java version

--- src/main/java/com/jjara/microservice/tag/configuration/MongoConfig.java
+++ src/main/java/com/jjara/microservice/tag/configuration/MongoConfig.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2022-present Jonathan Jara Morales
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.jjara.microservice.tag.configuration;
 
 import com.mongodb.reactivestreams.client.MongoClient;
@@ -13,16 +29,14 @@ import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 @Configuration
 public class MongoConfig {
 
-    @Autowired
-    private MongoClient mongoClient;
+    @Autowired private MongoClient mongoClient;
     @Value("${spring.data.mongodb.databaseName}") private String databaseName;
 
     /**
      * Gets the template that is used by spring to execute the distinct operations.
      * @return a mongo template instance
      */
-    @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate() {
+    @Bean ReactiveMongoTemplate reactiveMongoTemplate() {
         return new ReactiveMongoTemplate(mongoClient, databaseName);
     }
 }
